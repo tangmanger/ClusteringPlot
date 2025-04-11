dff8b0c776c56de969a8d9dca390042d8f6e0e1b
tangmanger <tangmanger@outlook.com>
2025-04-12 01:31:16 +0800

    fix  初始化

---
+++ ClusteringPlot/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using EasyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClusteringPlot
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            int level = 1;
+            int colum = 0;
+            List<string> cols = new List<string>();
+            List<ClusteringModel> clusteringModels = new List<ClusteringModel>();
+            for (int i = 0; i < 3; i++)
+            {
+                ClusteringModel clusteringModel = new ClusteringModel();
+                clusteringModel.Level = 1;
+                if (i < 2)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (clusteringModel.Clustering == null)
+                            clusteringModel.Clustering = new List<ClusteringModel>();
+                        ClusteringModel childClusteringModel = new ClusteringModel();
+                        childClusteringModel.Level = 2;
+                        childClusteringModel.Name = "第二级" + j;
+                        childClusteringModel.Col = colum;
+                        childClusteringModel.Clustering = new List<ClusteringModel>();
+                        if (j < 1)
+                        {
+                            for (int k = 0; k < 3; k++)
+                            {
+                                ClusteringModel childKClusteringModel = new ClusteringModel();
+                                childKClusteringModel.Level = 3;
+                                childKClusteringModel.Name = "第三极" + k;
+                                childKClusteringModel.Col = colum;
+                                childKClusteringModel.Clustering = new List<ClusteringModel>();
+                                if (k < 2)
+                                {
+                                    for (int m = 0; m < 2; m++)
+                                    {
+                                        ClusteringModel childKClusteringModel1 = new ClusteringModel();
+                                        childKClusteringModel1.Level = 4;
+                                        childKClusteringModel1.Name = "第四极" + k;
+                                        childKClusteringModel1.Col = colum;
+                                        childKClusteringModel.Clustering.Add(childKClusteringModel1);
+                                    }
+                                }
+                                childClusteringModel.Clustering.Add(childKClusteringModel);
+
+                            }
+                        }
+
+                        clusteringModel.Clustering.Add(childClusteringModel);
+                    }
+                }
+                else
+                {
+                    clusteringModel.Name = "第一级AAAA" + i;
+                    clusteringModel.Col = colum;
+
+
+
+                }
+                clusteringModels.Add(clusteringModel);
+
+
+            }
+            ClusteringModel clusteringModel1 = new ClusteringModel();
+            clusteringModel1.Clustering = clusteringModels;
+            aa.MaxTreeLevel = 2;
+            aa.Columns = cols;
+            aa.Clustering = clusteringModel1;
+        }
+    }
+}
---
+++ EasyPlot/Series/TreePlot.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace EasyPlot.Series
+{
+    public class ClusteringModel
+    {
+        public string Name { get; set; }
+        public List<ClusteringModel> Clustering { get; set; } = new List<ClusteringModel>();
+        public int Level { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public int Col { get; set; }
+    }
+    public class TreePlot : Control
+    {
+        public List<string> Columns
+        {
+            get { return (List<string>)GetValue(ColumnsProperty); }
+            set { SetValue(ColumnsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Columns.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColumnsProperty =
+            DependencyProperty.Register("Columns", typeof(List<string>), typeof(TreePlot), new PropertyMetadata(null));
+
+
+
+        public int MaxTreeLevel
+        {
+            get { return (int)GetValue(MaxTreeLevelProperty); }
+            set { SetValue(MaxTreeLevelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MaxTreeLevel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxTreeLevelProperty =
+            DependencyProperty.Register("MaxTreeLevel", typeof(int), typeof(TreePlot), new PropertyMetadata(1));
+
+        public double PreLevelStep { get; private set; }
+        public double PreLevelHeight { get; private set; }
+
+        private int i;
+
+        public double MaxTextWidth { get; private set; }
+        public double Levels { get; private set; }
+
+        public int TreeDepth { get; private set; }
+        public int NodeCount { get; private set; }
+        public double LineMaxWidth { get; private set; }
+        public ClusteringModel Clustering
+        {
+            get { return (ClusteringModel)GetValue(ClusteringProperty); }
+            set { SetValue(ClusteringProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Clustering.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ClusteringProperty =
+            DependencyProperty.Register("Clustering", typeof(ClusteringModel), typeof(TreePlot), new PropertyMetadata(new PropertyChangedCallback(ClusteringCallBack)));
+        private int depth;
+
+        private static void ClusteringCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                ClusteringModel clusteringModel = e.NewValue as ClusteringModel;
+                TreePlot clusteringMap = d as TreePlot;
+                if (clusteringMap != null)
+                {
+                    //计算树深
+                    clusteringMap.TreeDepth = clusteringMap?.GetTreeDepth(clusteringModel) ?? 1;
+                    //计算子节点数
+                    clusteringMap?.ExecuteLastNode(clusteringModel);
+                    //计算最大文本宽度
+                    clusteringMap?.GetMaxTextWidth(clusteringModel);
+
+                }
+                clusteringMap?.InvalidateVisual();
+            }
+        }
+        void GetMaxTextWidth(ClusteringModel root)
+        {
+            if (root == null) return;
+            if (!string.IsNullOrWhiteSpace(root.Name))
+            {
+                FormattedText formattedText = DrawText(root.Name);
+                double actualWidth = formattedText.WidthIncludingTrailingWhitespace;
+                MaxTextWidth = Math.Max(MaxTextWidth, actualWidth);
+            }
+            if (root.Clustering != null && root.Clustering.Count > 0)
+            {
+                foreach (var child in root.Clustering)
+                {
+                    GetMaxTextWidth(child);
+                }
+            }
+        }
+        public int GetTreeDepth(ClusteringModel root)
+        {
+            if (root == null) return 0;
+
+            int maxChildDepth = 0;
+            if (root.Clustering != null)
+            {
+                foreach (var child in root.Clustering)
+                {
+                    int childDepth = GetTreeDepth(child);
+                    maxChildDepth = Math.Max(maxChildDepth, childDepth);
+                }
+            }
+            //root.Level = maxChildDepth + 1;
+            return maxChildDepth + 1;
+        }
+        public void ExecuteLastNode(ClusteringModel root)
+        {
+            if (root != null)
+            {
+                if (root.Clustering != null && root.Clustering.Count > 0)
+                {
+                    foreach (var child in root.Clustering)
+                    {
+                        ExecuteLastNode(child);
+                    }
+                }
+                else
+                {
+                    NodeCount++;
+                }
+            }
+
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+
+            drawingContext.DrawRectangle(Background, null, new Rect(0, 0, this.ActualWidth, this.ActualHeight));
+            //先计算还有多少可以使用
+            LineMaxWidth = this.ActualWidth - this.MaxTextWidth;
+            //计算每个格的宽度
+            PreLevelStep = LineMaxWidth / (TreeDepth);
+            PreLevelHeight = this.ActualHeight / NodeCount;
+            i = 0;
+            depth = 0;
+            DrawClustering(Clustering, drawingContext, ActualWidth, this.ActualHeight, true);
+            base.OnRender(drawingContext);
+        }
+        FormattedText DrawText(string str)
+        {
+            var formattedText = new FormattedText(
+           str,
+           CultureInfo.CurrentCulture,
+             FlowDirection.LeftToRight,
+           new Typeface(FontFamily, FontStyle, FontWeight, FontStretch), 14, new SolidColorBrush((Color)Colors.Black), 1);
+            return formattedText;
+        }
+
+        private Point DrawClustering(ClusteringModel clustering, DrawingContext drawingContext, double width, double height, bool isRoot = false)
+        {
+            Point startPoint = new Point(0, 0);
+
+            if (clustering != null)
+            {
+                if (clustering.Clustering != null && clustering.Clustering.Count > 0)
+                {
+                    double minY = double.MaxValue;
+                    double maxY = double.MinValue;
+                    double x = 0;
+                    for (int i = 0; i < clustering.Clustering.Count; i++)
+                    {
+                        var childClustering = clustering.Clustering[i];
+                        if (childClustering.Clustering != null)
+                        {
+                            var newPoint = DrawClustering(childClustering, drawingContext, width, height);
+                            x = newPoint.X;
+                            if (newPoint.Y < minY)
+                            {
+                                minY = newPoint.Y;
+                            }
+                            if (newPoint.Y > maxY)
+                            {
+                                maxY = newPoint.Y;
+                            }
+
+
+                        }
+                    }
+                    startPoint = new Point(x, minY);
+                    var endPoint = new Point(x, maxY);
+                    var mindY = minY + (maxY - minY) / 2;
+                    //画闭合线
+                    drawingContext.DrawLine(new Pen(Brushes.Black, 1), startPoint, endPoint);
+                    if (!isRoot)
+                    {
+                        startPoint = new Point(LineMaxWidth - PreLevelStep * (TreeDepth - clustering.Level), mindY);
+                        endPoint = new Point(startPoint.X + PreLevelStep, startPoint.Y);
+                        drawingContext.DrawLine(new Pen(Brushes.Black, 1), startPoint, endPoint);
+                    }
+                }
+                else
+                {
+                    startPoint = new Point(LineMaxWidth - PreLevelStep * (TreeDepth - clustering.Level), 0 + PreLevelHeight * i);
+                    Point endPoint = new Point(startPoint.X + PreLevelStep, startPoint.Y);
+                    if (!string.IsNullOrWhiteSpace(clustering.Name))
+                    {
+                        FormattedText formattedText = DrawText(clustering.Name);
+                        double actualWidth = formattedText.WidthIncludingTrailingWhitespace;
+                        double actualHeight = formattedText.Height;
+                        Point point = new Point(width - actualWidth, 0 + actualHeight / 2 + PreLevelHeight * i);
+                        drawingContext.DrawText(formattedText, point);
+                        startPoint.Y = point.Y + actualHeight / 2;
+                        endPoint.X = endPoint.X + MaxTextWidth - actualWidth;
+                        endPoint.Y = startPoint.Y;
+                        if ((clustering.Clustering == null || clustering.Clustering.Count == 0) && clustering.Level < TreeDepth - 1)
+                        {
+                            var midWidth = LineMaxWidth - endPoint.X;
+                            endPoint.X = endPoint.X + midWidth + MaxTextWidth - actualWidth; 
+                        }
+
+                    }
+                    i++;
+                    drawingContext.DrawLine(new Pen(Brushes.Black, 1), startPoint, endPoint);
+                }
+            }
+            return startPoint;
+        }
+
+    }
+}
